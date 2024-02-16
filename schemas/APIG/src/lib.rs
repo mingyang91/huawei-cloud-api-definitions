cb2d594afdace48dc626a099c0c0a62654839e33
mingyang91 <mingyang91@qq.com>
2024-02-16 01:05:08 +0000

    chore: auto-update ${date}
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- schemas/APIG/src/lib.rs
+++ schemas/APIG/src/lib.rs
@@ -74,6 +74,8 @@ pub mod CreateAclStrategyV2;
 pub mod CreateAnApp;
 #[cfg(feature = "CreateAnAppV2")]
 pub mod CreateAnAppV2;
+#[cfg(feature = "CreateApi")]
+pub mod CreateApi;
 #[cfg(feature = "CreateApiAclBindingV2")]
 pub mod CreateApiAclBindingV2;
 #[cfg(feature = "CreateApiGroup")]
