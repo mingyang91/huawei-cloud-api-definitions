ccc5a5153e0c7df865de8a11b0bf0806c316e356
mingyang91 <mingyang91@qq.com>
2024-04-02 01:07:54 +0000

    chore: auto-update ${date}
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ schemas/IdentityCenterOIDC/src/lib.rs
@@ -0,0 +1,6 @@
+#[cfg(feature = "CreateToken")]
+pub mod CreateToken;
+#[cfg(feature = "RegisterClient")]
+pub mod RegisterClient;
+#[cfg(feature = "StartDeviceAuthorization")]
+pub mod StartDeviceAuthorization;
