fcee1acb394c48cdc5d48a7b15aa30b5d1489984
mingyang91 <mingyang91@qq.com>
2026-07-23 03:07:08 +0000

    chore: auto-update
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- schemas/UCS/src/lib.rs
+++ schemas/UCS/src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(feature = "CreateAddonInstance")]
 pub mod CreateAddonInstance;
+#[cfg(feature = "CreateClusterConf")]
+pub mod CreateClusterConf;
 #[cfg(feature = "CreateClusterGroupPolicyInstance")]
 pub mod CreateClusterGroupPolicyInstance;
 #[cfg(feature = "CreateClusterKubeconfig")]
